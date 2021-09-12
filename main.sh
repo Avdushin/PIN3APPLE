274440533865fb62106d2bfd9b0f24a1d6a57b96
Ananazz <54774559+Avdushin@users.noreply.github.com>
2021-09-13 00:06:59 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -87,13 +87,14 @@ ______ _____ _   _ _____  ___ ____________ _     _____
       echo "solus script is starting..."
       echo ""
       #!/bin/bash
-sudo eopkg it i3 i3blocks i3status i3-devel i3lock polybar kitty krita xkill fish ack vim neofetch feh rofi discord steam telegram gcolor3 lxappearance  flatpak xdg-desktop-portal-gtk font-awesome-4 font-awesome-ttf -y && sudo mkdir -p ~/.appz && sudo mkdir -p ~/.config/i3/ &&  sudo tar -C $HOME -xzf NoiseTorch_x64.tgz && sudo tar -xf sublime_text_build_4113_x64.tar.xz  -C ~/.appz && gtk-update-icon-cache && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && sudo cp config config-polybar ~/.config/i3/ && sudo cp -r polybar/ ~/.config/ && && sudo cp -r kitty/ ~/.config/ && sudo flatpak install flathub com.rafaelmardojai.Blanket -y && fish && sudo echo "# remove welcome text
+      #!/bin/bash
+     sudo eopkg it i3 i3blocks i3status i3-devel i3lock polybar kitty krita xkill fish ack vim neofetch feh rofi discord steam telegram gcolor3 lxappearance  flatpak xdg-desktop-portal-gtk font-awesome-4 font-awesome-ttf -y && sudo mkdir -p ~/.appz && sudo mkdir -p ~/.config/i3/ &&  sudo tar -C $HOME -xzf NoiseTorch_x64.tgz && sudo tar -xf sublime_text_build_4113_x64.tar.xz  -C ~/.appz && gtk-update-icon-cache && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && sudo cp config config-polybar ~/.config/i3/ && sudo cp -r polybar/ ~/.config/  && sudo cp -r kitty/ ~/.config/ && sudo flatpak install flathub com.rafaelmardojai.Blanket -y && fish && sudo echo "# remove welcome text
  set -g fish_greeting" > ~/.config/fish/config.fish && sudo echo "fish
      source /usr/share/defaults/etc/profile
      #noisetorch
      if [ -d "$HOME/.local/bin" ] ; then
          PATH="$HOME/.local/bin:$PATH"
-     fi" > ~/.bashrc  && sudo cp -r /walls/ ~/ && reboot && 
+     fi" > ~/.bashrc && sudo cp -r /walls/ ~/ && reboot 
       echo ""
       break
       ;;
