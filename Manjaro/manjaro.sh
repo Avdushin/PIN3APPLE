2f768ba0fcee8acb33008f9387e472c3a4fe61f8
Ananazz <54774559+Avdushin@users.noreply.github.com>
2021-09-13 17:13:09 +0300

    Update manjaro.sh

--- Manjaro/manjaro.sh
+++ Manjaro/manjaro.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-<<<<<<< HEAD
+     #!/bin/bash
 sudo pacman -Syu yay i3 i3blocks i3status i3lock polybar kitty krita fish ack vim neofetch flameshot feh rofi discord steam telegram-desktop gcolor3 lxappearance flatpak xdg-desktop-portal-gtk awesome-terminal-fonts -y && sudo mkdir -p ~/.appz && sudo mkdir -p ~/.config/i3/ && sudo mkdir -p ~/.local/share/Trash/files &&  sudo tar -C $HOME -xzf NoiseTorch_x64.tgz && sudo tar -xf sublime_text_build_4113_x64.tar.xz  -C ~/.appz && gtk-update-icon-cache && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && sudo cp Manjaro/config ~/.config/i3 && sudo cp -r polybar/ ~/.config/  && sudo cp -r kitty/ ~/.config/ && sudo flatpak install flathub com.rafaelmardojai.Blanket -y && fish && sudo echo "# remove welcome text
-=======
-sudo pacman -Syu yay i3 i3blocks i3status i3lock polybar kitty krita fish ack vim neofetch feh rofi discord steam telegram-desktop gcolor3 lxappearance flatpak xdg-desktop-portal-gtk awesome-terminal-fonts -y && sudo mkdir -p ~/.appz && sudo mkdir -p ~/.config/i3/ && sudo mkdir -p ~/.local/share/Trash/files &&  sudo tar -C $HOME -xzf NoiseTorch_x64.tgz && sudo tar -xf sublime_text_build_4113_x64.tar.xz  -C ~/.appz && gtk-update-icon-cache && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && sudo cp Manjaro/config ~/.config/i3 && sudo cp -r polybar/ ~/.config/  && sudo cp -r kitty/ ~/.config/ && sudo flatpak install flathub com.rafaelmardojai.Blanket -y && fish && sudo echo "# remove welcome text
->>>>>>> 21b5e4e2fcb6ed92064cafee8ec4b68310c03065
  set -g fish_greeting" > ~/.config/fish/config.fish && sudo echo "fish
      source /usr/share/defaults/etc/profile
      #noisetorch
