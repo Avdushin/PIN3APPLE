1d11908960f61be034c5148d73a8aa7ea104a291
Avdushin <avdushinbeaver1@gmail.com>
2021-10-20 18:28:50 +0300

    Uninstall script with progress bar

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,2 @@
-clear
-echo -e "\e[0;31mUNINSTALLING PIN3APPLE CONFIG...\e[0m"
-sudo rm -rf ~/.config/i3/config ~/.config/polybar/ ~/.config/kitty/ ~/.config/variety/ ~/.appz/ ~/walls/
-sudo eopkg rm i3 i3blocks i3status i3-devel i3lock polybar kitty krita xkill fish ack bottom vim neofetch variety feh flameshot rofi discord steam telegram gcolor3 lxappearance picom flatpak xdg-desktop-portal-gtk font-awesome-4 font-awesome-ttf -y 
-echo -e "\e[0;92mDONE!"
+rm -rf ~/.config/i3/config ~/.config/polybar/ ~/.config/kitty/ ~/.config/variety/ ~/.appz/ ~/walls/
+eopkg rm i3 i3blocks i3status i3-devel i3lock polybar kitty krita xkill fish ack bottom vim neofetch variety feh flameshot rofi discord steam telegram gcolor3 lxappearance picom flatpak xdg-desktop-portal-gtk font-awesome-4 font-awesome-ttf -y
