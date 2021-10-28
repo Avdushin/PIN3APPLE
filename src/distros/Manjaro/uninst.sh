e78af139b541d4e2031013b41d4e45b2bbc2591d
Avdushin <avdushinbeaver1@gmail.com>
2021-10-29 00:12:38 +0300

    sudo mode added to distros//uninstall.sh

--- src/distros/Manjaro/uninst.sh
+++ src/distros/Manjaro/uninst.sh
@@ -1,2 +1,2 @@
 sudo pacman -Rs yay i3 i3blocks i3status i3lock polybar kitty krita fish ack vim neofetch flameshot variety feh rofi discord python-pip steam telegram-desktop gcolor3 lxappearance picom flatpak xdg-desktop-portal-gtk awesome-terminal-fonts -y
-rm -rf ~/.config/i3/config ~/.config/polybar/ ~/.config/kitty/ ~/.config/variety/ ~/.appz/ ~/walls/ ~/.fonts/coins.otf ~/.fonts/coins.svg
+sudo rm -rf ~/.config/i3/config ~/.config/polybar/ ~/.config/kitty/ ~/.config/variety/ ~/.appz/ ~/walls/ ~/.fonts/coins.otf ~/.fonts/coins.svg
