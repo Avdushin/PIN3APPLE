0b81b3736e567b5be9be18a971dba49445d00d61
Avdushin <avdushinbeaver1@gmail.com>
2021-10-29 00:52:45 +0300

    Manjaro | uninstall fixed : path & some lines

--- src/distros/Manjaro/uninst.sh
+++ src/distros/Manjaro/uninst.sh
@@ -1,2 +1,2 @@
-sudo pacman -Rs yay i3 i3blocks i3status i3lock polybar kitty krita fish ack vim neofetch bottom flameshot variety feh rofi discord python-pip steam telegram-desktop gcolor3 lxappearance picom flatpak xdg-desktop-portal-gtk awesome-terminal-fonts -y
+sudo pacman -Rs i3 i3blocks i3status i3lock polybar kitty krita fish ack vim bottom neofetch flameshot variety feh rofi discord python-pip steam telegram-desktop gcolor3 lxappearance picom flatpak xdg-desktop-portal-gtk awesome-terminal-fonts -y
 sudo rm -rf ~/.config/i3/config ~/.config/polybar/ ~/.config/kitty/ ~/.config/variety/ ~/.appz/ ~/walls/ ~/.fonts/coins.otf ~/.fonts/coins.svg
