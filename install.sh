5cb5ac3484ff93bd1b4d930f822c4d934d255f80
Avdushin <avdushinbeaver1@gmail.com>
2022-01-04 21:00:50 +0300

    Updated pip -> python -m install < package >

--- install.sh
+++ install.sh
@@ -12,9 +12,12 @@ sudo dnf update -y
 clear
 echo -e "\e[0;92mInstalling requirements...\e[0;1;33m"
 /usr/bin/python3.9 -m pip install --upgrade pip
-pip3 install rich
-pip3 install pyfiglet
-pip3 install tqdm
+#pip3 install rich
+#pip3 install pyfiglet
+#pip3 install tqdm
+python -m pip install rich
+python -m pip install pyfiglet
+python -m pip install tqdm
 echo -e "\e[0;92mDONE!"
 sleep 2
 clear
