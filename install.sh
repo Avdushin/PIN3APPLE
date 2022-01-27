e4649b5b027834d0798f7439831dcc593381a0ae
Avdushin <avdushinbeaver1@gmail.com>
2022-01-27 19:54:47 +0300

    Fixed pip3 reqs at install.sh && .gitattributes

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+sudo pacman -S python-pip --noconfirm
+yay -S python39 --noconfirm
+clear
 echo 'Iinstalling requirements...'
 pip3 install -r src/requirements.txt
-clear
-pip3.10 install -r src/requirements.txt
 clear
 echo 'Successful installation!'
 sh pineapple.sh
